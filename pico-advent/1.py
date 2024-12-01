48fe60e4a66e76af0e84970ea0af680931dd6f22
Tomas Belada <tomas@belada.net>
2024-12-01 21:34:52 +0100

    day-1

---
+++ pico-advent/1.py
@@ -0,0 +1,13 @@
+from machine import Pin
+from utime import sleep
+
+led = Pin(25, Pin.OUT)
+led.toggle()
+
+print("This is my piko blinkink")
+
+# for i in range(10):
+#     led.toggle()
+#     sleep(0.5)
+
+
---
+++ pico-advent/temperature.py
@@ -0,0 +1,24 @@
+from machine import ADC
+import time
+
+# Initialize ADC for temperature sensor
+sensor_temp = ADC(4)
+conversion_factor = 3.3 / (65535)
+
+while True:
+    try:
+      # Read raw ADC value
+      reading = sensor_temp.read_u16()
+      
+      # Convert raw value to voltage
+      voltage = reading * conversion_factor
+      
+      # Convert voltage to temperature in Celsius
+      temperature = 27 - (voltage - 0.706) / 0.001721
+      
+      print("Temperature: {:.2f}°C".format(temperature))
+      
+      # Wait for a second before reading again
+      time.sleep(1)
+    except KeyboardInterrupt:
+        break
